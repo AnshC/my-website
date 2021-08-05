4b246380a001ad243a66a1a4ba192ebf0315bb70
AnshC <63086888+AnshC@users.noreply.github.com>
2021-08-05 17:39:00 +0530

    images added

--- src/components/design.jsx
+++ src/components/design.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faArrowCircleLeft} from '@fortawesome/free-solid-svg-icons'
 
 export default function Design(){
     return(
@@ -6,7 +9,17 @@ export default function Design(){
             <div className="hero">
                 <div className="content">
                     <h1>Welcome to my Design Portfolio!</h1>
+                    <Link to="/" className="link"><FontAwesomeIcon icon={faArrowCircleLeft}/> Go to main website</Link>
                 </div>
+            </div>
+            <div className="cards">
+                <div className="card" style={{backgroundImage: "url(/img/design/cat.jpg)"}}></div>
+                <div className="card" style={{backgroundImage: "url(/img/design/diamond.png)"}}></div>
+                <div className="card" style={{backgroundImage: "url(/img/design/head.png)"}}></div>
+                <div className="card" style={{backgroundImage: "url(/img/design/jellyfish.png)"}}></div>
+                <div className="card" style={{backgroundImage: "url(/img/design/profile.png)"}}></div>
+                <div className="card" style={{backgroundImage: "url(/img/design/teenage.png)"}}></div>
+                <div className="card" style={{backgroundImage: "url(/img/design/wolf.jpg)"}}></div>
             </div>
         </div>
     )

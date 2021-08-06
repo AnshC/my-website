1bec3dea20ed7e4a61e42c21125295d87f026ca4
AnshC <63086888+AnshC@users.noreply.github.com>
2021-08-06 15:19:16 +0530

    6th august 2021

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ export default function Footer(){
     return(
         <div className="footer">
             <div className="content">
-                <h1 className="update">Last Updated On: 5th August 2021</h1>
+                <h1 className="update">Last Updated On: 6th August 2021</h1>
             </div>
         </div>
     )

fd5a88b1cdadd2f6fab9aa15cf43271c3ea78b1d
Ansh Chauhan <achauhan4580@gmail.com>
2021-10-09 20:23:33 +0530

    landing message updated

--- src/components/blog/travel/landing.jsx
+++ src/components/blog/travel/landing.jsx
@@ -1,7 +1,7 @@
 export default function TravelPageLanding () {
     return (
-        <div className="travel-landing">
-            
+        <div className="travel-landing main">
+            <h1 sx={{color: 'text'}}>Page Under Construction...</h1>
         </div>
     )
 }
